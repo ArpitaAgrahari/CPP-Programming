69c3673e8086833265b8b28db4c032722740308d
Arpita Agrahari <115877354+ArpitaAgrahari@users.noreply.github.com>
2023-09-01 05:28:22 -0700

    Add files via upload

---
+++ Ragu_personalRecords.cpp
@@ -0,0 +1,39 @@
+// You are using GCC
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+class Person{
+    private:
+    string name;
+    int age;
+    string gender;
+    
+    public:
+    void initialize(string n,int a, string g){
+        name = n;
+        age = a;
+        gender = g;
+    }
+    
+    void display(){
+        transform(name.begin(), name.end(), name.begin(), ::toupper);
+        transform(gender.begin(), gender.end(). gender.begin(), ::toupper);
+        
+        cout << name <<" "<< age << " " << gender <<endl;
+    }
+};
+
+int main()
+{
+    Person person;
+    string name, gender;
+    int age;
+    cin>> name >> age >> gender;
+    
+    person.initialize(name, age, gender);
+    person.display();
+    
+    return 0;
+}
