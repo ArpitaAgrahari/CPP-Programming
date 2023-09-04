91921ea15a99ee5b74d606bba9e132e0ef2fff8d
Arpita Agrahari <115877354+ArpitaAgrahari@users.noreply.github.com>
2023-09-04 10:52:15 -0700

    Add files via upload

---
+++ PhoneNumber_telecommunication.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<string>
+#include<cctype>
+using namespace std;
+
+class phone_number
+{
+    private:
+    std::string num;
+    
+    inline bool check_num(std::string num)
+    {
+        if(num.length() != 10)
+        {
+            return false;
+        }
+        for(char c: num)
+        {
+            if(!isdigit(c))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public:
+    void input_num()
+    {
+        std::cout <<"";
+        std::cin>>num;
+        
+        if(check_num(num))
+        {
+            std::cout <<"Your entered number is: "<<num<<std::endl;
+    
+        }
+        else{
+            std::cout<<"Entered phone number is wrong"<<std::endl;
+            if(!num.empty()){
+                std::cout<<"Your enetred number is: "<<num<<std::endl;
+            }
+        }
+    }
+};
+
+int main(){
+    phone_number num;
+    num.input_num();
+    return 0;
+}
